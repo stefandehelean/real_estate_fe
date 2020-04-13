8a5cec82a3d753c1405bb424d0ba77c8b13c02b4
stefan_dehelean <stefan.dehelean@avangarde.software.com>
2020-04-13 11:36:51 +0300

    fixed fb ouath redirect

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,7 +2,7 @@ const env = 'dev';
 const apiUrl = 'https://real-estate-app-stefan.herokuapp.com';
 const fbAppId = '333179914323347';
 const fbAppSecret = '4662a1e472586d9c64b2156e82417f34';
-const fbRedirectURL = 'https://real-estate-app-stefan-fe.herokuapp.com';
+const fbRedirectURL = 'https://real-estate-app-stefan-fe.herokuapp.com/facebookauth';
 
 export const dev = {
   'env': env,
