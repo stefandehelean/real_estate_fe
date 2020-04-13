2f0913a54ed5d5398df4b8f59306e86754f98246
stefan_dehelean <stefan.dehelean@avangarde.software.com>
2020-04-13 10:04:16 +0300

    env set to dev

--- src/config/config.js
+++ src/config/config.js
@@ -3,14 +3,14 @@ import {prod}  from './prod';
 import {test}  from './test';
 
 export const config = (() => {
-  switch (process.env.NODE_ENV) {
+  /*switch (process.env.NODE_ENV) {
     case 'dev':
       return dev;
     case 'prod':
       return prod;
     case 'test':
       return test;
-    default:
-      return dev;
-  }
+    default:*/
+  return dev;
+  /*}*/
 })();
